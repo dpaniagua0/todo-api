6d56478c418ee9078b8bc09d3aa540b4a3314727
dpaniagua0 <1273602+dpaniagua0@users.noreply.github.com>
2018-05-08 06:57:57 -0500

    add test route for delete /users/me/token

--- server/tests/server.test.js
+++ server/tests/server.test.js
@@ -280,3 +280,23 @@ describe('POST /users/login', () => {
       });
   });
 });
+
+describe('DELETE /users/me/token', () => {
+  it('should delete token and logout user', (done) => {
+    var email = users[0].email;
+    var token = users[0].tokens[0].token;
+    request(app)
+      .delete('/users/me/token')
+      .set('x-auth', token)
+      .expect(200)
+      .end((err, res) => {
+        if(err) {
+          return done(err);
+        }
+        User.findById(users[1]._id).then((user) => {
+          expect(user.tokens.length).toBe(0);
+          done();
+        }).catch((e) => done(e));
+      })
+  });
+});
